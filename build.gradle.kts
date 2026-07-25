a75474f3570f3f1060ae450940ee807e2e130b71
Hisha <officialhisha@protonmail.com>
2026-07-25 14:53:01 +0200

    Updated to compile against Minecraft 26.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,8 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
-    compileOnly("com.nexomc:nexo:1.23")
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("com.nexomc:nexo:1.26.0")
 }
 
 java {
@@ -23,7 +23,7 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("26.1.2")
+        minecraftVersion("26.2")
         jvmArgs("-Xms2G", "-Xmx2G", "-Dcom.mojang.eula.agree=true")
     }
 
